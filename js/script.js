615aa6b4d1514ba70c26e0cc956e0dc7e2b9ddad
Firstname Lastname <githubemail@domain.com>
2022-12-06 13:32:28 -0700

    adds working js

--- js/script.js
+++ js/script.js
@@ -26,34 +26,54 @@
 // IPO-Input -> Process -> Output
 
 // // Constants and Variables
-const API_URL = "https://api.nasa.gov/planetary/apod?";
+const NASA_URL = "https://api.nasa.gov/planetary/apod?";
 const API_Key = "api_key=J52YIQrpU9avynBAXeQP7R2ytiaeUKR9Ir64XGhM";
+const API_URL = "https://api.nasa.gov/planetary/apod?api_key=J52YIQrpU9avynBAXeQP7R2ytiaeUKR9Ir64XGhM";
 
 // // Cache DOM element reference
-const $image = $("#image");
-const $input= $('input[type="date"]')
-const $form= $('form');
+// const $image = $("#image");
+// const $input= $('input[type="text"]')
+// const $form= $('form');
+const $submit= $('#submit')
 
-// // Register Event Listeners
-$form.on('submit', getData)
+// // // Register Event Listeners
+// $form.on('submit', getData)
 
-// // Functions
-function getData() {
-    $.ajax(API_URL).then(function(data){
-    console.log(data);
-    }, function(error) {
-    console.log(error);
+// // // Functions
+// function getData(event) {
+//     // event.preventDefault();
+//     date= $input.val();
+//     $.ajax(API_URL + "date=" +date+ "&" + API_Key).then(function(data){
+//     console.log(data);
+//     $("#image").attr(`src= "event.hdurl"`)
+// },
+
+// function(error) {
+//     console.log(error);
+//     });
+
+// }
+
+// getData()
+
+
+$(function(){  
+
+    $.ajax({
+      url: API_URL,
+      function(error){
+        console.log(error)
+      }
     });
-}
-
-function getData() {
-    var data = $input.val();
-    console.log(date)
-}
-
-
-
-
-// + "date=" +date+ "&" + API_Key
-// vent.preventDefault();
-//     
+    $submit.on("click",function(){
+      var date = $("input").val();
+  
+      console.log(date)
+  
+      $.get(NASA_URL + "date="+date+ "&"+ API_Key, 
+      
+    function(event){
+        $("img").attr("src", event.hdurl)
+      })
+    })
+})
